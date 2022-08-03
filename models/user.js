a0a86ab2ae6aac1ac09a7816f7bf6ad0d19f09f7
Parker McKillop <mckilpar000@hotmail.com>
2022-08-03 13:22:35 -0700

    added basic user model

---
+++ models/user.js
@@ -0,0 +1,54 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require("bcrypt")
+const sequelize = require('../config/connection');
+
+class User extends Model { }
+
+User.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+
+    },
+
+    first_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+
+    last_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+
+    birthday: {
+        type: DataTypes.DATEONLY,
+        allowNull: false
+    },
+
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [8, 32]
+        }
+    }
+}, {
+    sequelize,
+    hooks: {
+        beforeCreate: userObj => {
+            userObj.password = bcrypt.hashSync(userObj.password, 4);
+            return userObj;
+        }
+    }
+});
+
+module.exports = User
